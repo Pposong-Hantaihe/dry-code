a0528853cd9fed793db4a920fe036c189a1dea97
Hantaehyuk <hantaihe@naver.com>
2022-07-08 12:10:02 +0900

    :white_check_mark: Test dry-code action

---
+++ src/code2.js
@@ -0,0 +1,16 @@
+console.log("Hello code!");
+console.log("Hello code!");
+console.log("Hello code!");
+console.log("Hello code!");
+console.log("Hello code!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
