620ecde4fe2107e2a741eeda24af905a58c83f25
Hantaehyuk <hantaihe@naver.com>
2022-07-09 22:04:26 +0900

    :sparkles: Add createComment function

--- index.js
+++ index.js
@@ -6,23 +6,39 @@ function exec(command){
   return execSync(command, {encoding: 'utf8'}).trim();
 }
 
-try {
-  const options = core.getInput('options');
-  const arguments = core.getInput('arguments');
+async function run() {
+  try {
+    const option = core.getInput('option');
+    const argument = core.getInput('argument');
+    const token = core.getInput('Token');
+    const context = github.context;
+    const { pull_request } = context.payload;
+  
+    const result = exec(`npx jscpd ${option} ${argument} | sed 's/\x1b\[[0-9;]*m//g'`);
+    const cloneFound = result.split("┌────────────┬────────────────┬─────────────┬──────────────┬──────────────┬──────────────────┬───────────────────┐\n");
+  
+    const chart = cloneFound[1].split("│");
+    let Formats = [];
+    for (let i = 0; i < chart.length; i++){
+      if(chart[i++] === "\n├────────────┼────────────────┼─────────────┼──────────────┼──────────────┼──────────────────┼───────────────────┤\n")
+        Formats.push(chart.slice(i, i+7).map((value) => {
+          return value.trim();
+        }));
+    }
 
-  const result = exec(`npx jscpd ${options} ${arguments} | sed 's/\x1b\[[0-9;]*m//g'`);
-  const cloneFound = result.split("┌────────────┬────────────────┬─────────────┬──────────────┬──────────────┬──────────────────┬───────────────────┐\n");
-  console.log(cloneFound[0]);
+    const blocks = cloneFound[0];
+    const graph = "Graph:\n\n"
+    const body = blocks + graph + Formats; // Formats => graph_url;
 
-  const chart = cloneFound[1].split("│");
-  let Formats = [];
-  for (let i = 0; i < chart.length; i++){
-    if(chart[i++] === "\n├────────────┼────────────────┼─────────────┼──────────────┼──────────────┼──────────────────┼───────────────────┤\n")
-      Formats.push(chart.slice(i, i+7).map((value) => {
-        return value.trim();
-      }));
+    const octokit = github.getOctokit(token);
+    await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: body
+    });
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  console.log(Formats);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
