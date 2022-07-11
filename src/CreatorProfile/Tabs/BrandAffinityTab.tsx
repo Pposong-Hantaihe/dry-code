e2560400ee3eb3ea081b5e9ea851234e2ecc280d
yongwookLee <yongw00k@g.skku.edu>
2022-07-11 21:31:40 +0900

    test

--- src/CreatorProfile/Tabs/BrandAffinityTab.tsx
+++ src/CreatorProfile/Tabs/BrandAffinityTab.tsx
@@ -26,6 +26,27 @@ const BrandAffinityTab = () => {
           <RecentVideos />
         </Grid>
       </Grid>
+      <Grid container spacing={2} >
+        <Grid item xs={12} md={6} >
+          <Typography variant="h6" mb={1}>
+            Brand Affinity
+          </Typography>
+          <Card variant="outlined">
+            <WordCloud colors={["#ff77aa", "#ff99cc", "#ffbbee", "#ff5588", "#ff3377"]}/>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6} >
+          <Typography variant="h6" mb={1}>
+            Interest Graph
+          </Typography>
+          <Card variant="outlined">
+            <WordCloud colors={["#0FC2C0", "#0CABA8", "#008F8C", "#015958", "#023535"]}/>
+          </Card>
+        </Grid>
+        <Grid item xs={12} >
+          <RecentVideos />
+        </Grid>
+      </Grid>
     </div>
   );
 };
