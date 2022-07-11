16aba579399ef5b7eb37e2fa8a9223a6a861c69a
yongwookLee <yongw00k@g.skku.edu>
2022-07-11 17:13:51 +0900

    :white_check_mark: Add test cases

---
+++ src/code copy 4.js
@@ -0,0 +1,15 @@
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Hello code!");
+console.log("Hello code!");
+console.log("Hello code!");
+console.log("Hello world!");
+console.log("Hello world!");
